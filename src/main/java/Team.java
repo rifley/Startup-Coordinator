94798b1781a443e88420f658e538729058a64f72
Riley Watts <rileywatts.yes@gmail.com>
2017-03-24 08:40:04 -0700

    add test/method to add new team instances to arraylist

--- src/main/java/Team.java
+++ src/main/java/Team.java
@@ -1,3 +1,21 @@
+import java.util.List;
+import java.util.ArrayList;
+
+
 public class Team {
-  
+  private String mTeamName;
+  private static List<Team> instances = new ArrayList<Team>();
+
+   public Team(String name) {
+     mTeamName = name;
+     instances.add(this);
+   }
+
+   public String getName() {
+     return mTeamName;
+   }
+
+   public static List<Team> all() {
+     return instances;
+   }
 }
--- src/test/java/TeamTest.java
+++ src/test/java/TeamTest.java
@@ -5,8 +5,20 @@ public class TeamTest {
 
   @Test
   public void Team_instantiatesCorrectly_true() {
-    Team newTeam = new Team();
+    Team newTeam = new Team("");
     assertEquals(true, newTeam instanceof Team);
   }
+  @Test
+  public void Team_instantiatesWithString_String() {
+    Team newTeam = new Team("Lightning");
+    assertEquals("Lightning", newTeam.getName());
+  }
 
+  @Test
+  public void Team_returnsAllInstances_true() {
+    Team oneTeam = new Team("Lightning");
+    Team twoTeam = new Team("Thunder");
+    assertEquals(true, Team.all().contains(oneTeam));
+    assertEquals(true, Team.all().contains(twoTeam));
+  }
 }
